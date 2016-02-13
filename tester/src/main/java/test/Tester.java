2975971a1122db252225bd66fa20e8df660db393
alex <rojkov@gmail.com>
2016-02-12 17:01:13 -0800

    size fix

--- tester/src/main/java/test/Tester.java
+++ tester/src/main/java/test/Tester.java
@@ -18,7 +18,7 @@ public class Tester implements Runnable
    *
    * max byte[] in the session will be 1024 * m * request-number
    */
-  private final static int m = 1024 * 32;
+  private final static int m = 2;
 
   private long n = 1024 * 1024 * requestCount;
 
